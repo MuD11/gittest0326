e8ce693db57edd80de5d780407440ca852395b8c
Mu JianJun <599181142@qq.com>
2022-03-26 20:39:55 +0800

    push commit1

--- src/main/java/com/git/gitTest.java
+++ src/main/java/com/git/gitTest.java
@@ -9,5 +9,6 @@ public class gitTest {
         System.out.println("hot.fix");
         System.out.println("master fix");
         System.out.println("hot.fix2");
+        System.out.println("push github");
     }
 }
